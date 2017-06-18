1261889c60f28357b1af204fa7ba4310e57cb9ef
6851 <6851@192.168.178.20>
2017-06-18 21:43:58 +0200

    Gebruiker en rechten toevoegen

--- dbinit.sql
+++ dbinit.sql
@@ -1,5 +1,11 @@
+//Database creation
 CREATE SCHEMA adresboek;
 
+//Create user and rights
+CREATE USER 'adresboek'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';
+GRANT USAGE ON *.* TO 'adresboek'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `adresboek`.* TO 'adresboek'@'localhost';
+
+//create persoon table
 CREATE TABLE adresboek.persoon
 (
   id       NUMERIC(11) PRIMARY KEY,
