fedbb12ced57f42d0001a75310a3f5d5440efb06
Chris lutje Spelberg <chris@spelberg.nl>
2017-06-18 22:01:25 +0200

    Database init file uitgebreid met rechten

--- dbinit.sql
+++ dbinit.sql
@@ -1,11 +1,8 @@
-//Database creation
 CREATE SCHEMA adresboek;
 
-//Create user and rights
-CREATE USER 'adresboek'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';
-GRANT USAGE ON *.* TO 'adresboek'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `adresboek`.* TO 'adresboek'@'localhost';
+CREATE USER IF NOT EXISTS adresboek IDENTIFIED BY 'adresboek';
+GRANT ALL ON adresboek.* TO 'adresboek'@'%';
 
-//create persoon table
 CREATE TABLE adresboek.persoon
 (
   id       NUMERIC(11) PRIMARY KEY,
